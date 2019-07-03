816605fa98b1fb83d5d10b492957ada5daa5ed4c
Dariia Zhyr <darzhyr@gmail.com>
2019-07-03 15:28:33 +0300

    add responsive slidebar

--- src/main.js
+++ src/main.js
@@ -13,22 +13,35 @@ $(document).ready(function(){
           {
             breakpoint: 1224,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 3,
               slidesToScroll: 1,
               infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 640,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
+              dots: true,
               centerMode: false
             }
           },
           {
-            breakpoint: 360,
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              centerMode: true
+            }
+          },
+          {
+            breakpoint: 940,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true,
+              centerMode: false
+            }
+          },
+          {
+            breakpoint: 640,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
